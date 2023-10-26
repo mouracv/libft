90d63da69fd4e88ed47d39655b63c3221bf932d7
Alexsandro Pereira Moreira <aleperei@c2r10s1.42lisboa.com>
2023-10-26 12:35:49 +0100

    Ok

---
+++ ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleperei <aleperei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 10:20:03 by aleperei          #+#    #+#             */
+/*   Updated: 2023/05/09 15:23:05 by aleperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t n, size_t size)
+{
+	void	*dup;
+	size_t	total;
+
+
+	total = n * size;
+	dup = malloc(total);
+	if (!dup)
+		return (NULL);
+	ft_memset(dup, 0, total);
+	return (dup);
+}
---
+++ ft_strncmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleperei <aleperei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 09:49:18 by aleperei          #+#    #+#             */
+/*   Updated: 2023/04/19 12:06:15 by aleperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned int	i;
+	unsigned char	str1;
+	unsigned char	str2;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n - 1)
+		i++;
+	if (n == 0)
+		return (0);
+	else
+	{
+		str1 = s1[i];
+		str2 = s2[i];
+		return (str1 - str2);
+	}
+}
